9d5d5a162671cba2c94beed285f02af60e18fd42
S.Agalya <agalyaishere@gmail.com>
2025-06-12 21:17:07 +0530

    done with user registrations and validation

--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.UserRegistrationPage;
+import utils.DataProviders;
 
 public class UserRegistrationTest {
     UserRegistrationPage userRegistrationPage;
@@ -15,8 +16,23 @@ public class UserRegistrationTest {
 
         userRegistrationPage= new UserRegistrationPage(driver);
     }
-    @Test(priority = 1)
-    public void registerTest(){
-        userRegistrationPage.registerUser();
+    @Test(priority = 1, dataProvider = "registrationData", dataProviderClass = DataProviders.class)
+    public void registerTest(String username, String email, String password,
+                             String firstname, String lastname, String company,
+                             String address, String state, String city, String zipcode, String phone){
+
+        userRegistrationPage.registerUser(username,email);
+        if (userRegistrationPage.isEmailAlreadyExistsDisplayed()) {
+            System.out.println("Email already exists. Cannot register again.");
+        } else {
+            userRegistrationPage.registerFormFill(password, firstname, lastname, company, address, state, city, zipcode, phone);
+
+            if (userRegistrationPage.isAccountCreatedDisplayed()) {
+                System.out.println("Account created successfully.");
+            } else {
+                System.out.println("Account creation failed.");
+            }
+        }
+
     }
 }
